574b4edba8c4cf5b00893abf656331a088921237
Grant Richmond <mail@grant.codes>
2024-02-27 18:04:49 +0100

    refactor: improve some form styles

--- src/scss/elements.stories.ts
+++ src/scss/elements.stories.ts
@@ -66,6 +66,9 @@ export const Select: Story = {
   render: ({ text }) => html`<select>
     <option value="1">Option 1</option>
     <option value="2">Option 2</option>
+    <option value="3">Option 3</option>
+    <option value="4">Option 4</option>
+    <option value="5">Option 5</option>
   </select>`,
 }
 
