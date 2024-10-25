31aded67818decb2b47cc941cbf41ed4d595bc90
Grant Richmond <mail@grant.codes>
2024-10-25 10:39:55 +0200

    build: update vite build config

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,41 @@
 // vite.config.js
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  build: {
-    // lib: {
-    //   entry: [
-    //     resolve(__dirname, 'src/main.ts'),
-    //     // resolve(__dirname, 'src/react.ts'),
-    //     // resolve(__dirname, 'src/scss/base.scss'),
-    //     // resolve(__dirname, 'src/scss/themes/grantcodes.scss'),
-    //   ],
-    //   name: 'GrantCodesUI',
-    // },
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'src/main.ts'),
-        react: resolve(__dirname, 'src/react.ts'),
-        base: resolve(__dirname, 'src/scss/base.scss'),
-        grantcodes: resolve(__dirname, 'src/scss/themes/grantcodes.scss'),
-      },
-      external: ['react'],
-      output: {
-        assetFileNames: `[name].[ext]`,
-        entryFileNames: `[name].js`,
-        inlineDynamicImports: false,
-        globals: {
-          react: 'React',
-        },
-      },
-    },
-    cssCodeSplit: true,
-  },
-})
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, "src/main.ts"),
+				react: resolve(__dirname, "src/react.ts"),
+				"css/base": resolve(__dirname, "src/scss/base.scss"),
+				"css/themes/grantcodes": resolve(
+					__dirname,
+					"src/scss/themes/grantcodes.scss",
+				),
+			},
+			external: [
+				"react",
+				"lit",
+				"@lit/react",
+				"lit/decorators.js",
+				"lit/static-html.js",
+			],
+			output: {
+				assetFileNames: "[name].[ext]",
+				entryFileNames: "[name].js",
+				inlineDynamicImports: false,
+				preserveModules: true,
+				preserveModulesRoot: "src",
+				format: "es",
+				interop: "esModule",
+				globals: {
+					react: "React",
+				},
+			},
+			treeshake: false,
+			preserveEntrySignatures: "allow-extension",
+		},
+		cssCodeSplit: true,
+	},
+});
